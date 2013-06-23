052eedf5d807064796cbc55ecfc88af7e9cedc3e
pmhargisNG10 <pmhargis@gmail.com>
2013-06-22 23:13:10 -0500

    Update to App to run HelloWorld in Spring

--- SpringExamples/src/main/java/com/mkyong/common/App.java
+++ SpringExamples/src/main/java/com/mkyong/common/App.java
@@ -1,13 +1,18 @@
-package com.mkyong.common;
-
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+package com.mkyong.common;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+ 
+public class App {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"Spring-Module.xml");
+ 
+		HelloWorld obj = (HelloWorld) context.getBean("helloBean");
+		obj.printHello();
+	}
 }
